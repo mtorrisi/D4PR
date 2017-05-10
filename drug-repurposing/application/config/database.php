411f9425d5173b5b11c0fd662f33103a9166c75c
mtorrisi <torrisi.mario@gmail.com>
2017-05-10 12:46:09 +0200

    Change mysql driver library.

--- drug-repurposing/application/config/database.php
+++ drug-repurposing/application/config/database.php
@@ -50,9 +50,9 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'mrnicholas';
+$db['default']['password'] = 'super-secret-passwd';
 $db['default']['database'] = 'd4pr';
-$db['default']['dbdriver'] = 'mysql';
+$db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
